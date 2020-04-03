ce7f58da22a300aba466f0fe73c62314c7a6995b
Adam-Sawyer <44009459+Adam-Sawyer@users.noreply.github.com>
2020-04-03 18:37:48 -0500

    Wrote basic program except for alarm
    I believe that this is functioning properly. Can some of you double check with the simulator? It still needs the alarm to be implemented. I added a SYS_ALRM function but it has nothing in it at the moment.

--- Project1/Project1/main.asm
+++ Project1/Project1/main.asm
@@ -5,8 +5,67 @@
 ; Author : Adam
 ;
 
+LDI R16, 0x00 ;Set register 16 to 0 initially. This register will store the current count.
+LDI R17, 0x01 ;Set register 17 to 1 initially. This register will store the increment amount.
+LDI R18, 0xFF
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+OUT DDRD, R18 ;Port D in output mode
+OUT PORTD, R18 ;Turn off LEDS (active low)
+
+OUT DDRA, R19 ; Port A in input mode
+OUT PORTA, R18
+	 
+CHECK_SW1:
+    SBIC PINA, 0 ; Skip next instruction if PA0 gets a 0
+    RJMP CHECK_SW2
+    RJMP INC_CNT
+
+CHECK_SW2:
+	SBIC PINA, 1 ; Skip next instruction if PA0 gets a 0
+    RJMP LIM_CHK
+    RJMP DEC_CNT
+
+INC_CNT:		 ; Increases the count by the value of the counter
+	ADD R16, R17
+	RJMP CHK_VAL
+
+DEC_CNT:		 ; Decreases the count by the value of the counter
+	SUB R16, R17
+	RJMP CHK_VAL
+
+CHK_VAL:		 ; Checks if max value has been reached
+	SBIS PIND, 7
+	RJMP AT_MIN
+	SBIS PIND, 5
+	RJMP AT_MAX
+	RJMP SET_LED
+
+SET_LED:	       ; Turns on the LEDs according to value stored in R16 (The count register) 
+	MOV R18, R16   ; R18 is used as a temporary register to store the count value
+	COM R18        ; Takes Ones compliment because LEDs are active low
+	OUT PORTD, R18 ; Turns LEDs on 
+	RJMP CHECK_SW1
+
+AT_MIN:			 ; Sets value of counter to 31
+	LDI R16, 0x1F
+	;RJMP SYS_ALRM
+
+AT_MAX:			 ; Sets value of counter to 0
+	LDI R16, 0x00
+	;RJMP SYS_ALRM
+
+SYS_ALRM:		 ; Sets off the system alarm
+	RJMP CHECK_SW1
+	
+
+QDELAY: 
+	LDI R23, 255
+	AGAIN3: LDI R22, 255
+			AGAIN2:LDI R21, 25 
+				AGAIN1:NOP 
+					   DEC R21 
+					   BRNE AGAIN1          
+				DEC R22
+				BRNE AGAIN2
+			DEC R23
+			BRNE AGAIN3
